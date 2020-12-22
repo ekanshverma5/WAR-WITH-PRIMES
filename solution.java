76b8ffacb1f915bffe585190cff9980097e6b16b
ekanshverma5 <60564078+ekanshverma5@users.noreply.github.com>
2020-12-22 13:34:47 +0530

    updated solution

--- solution.java
+++ solution.java
@@ -41,7 +41,11 @@ public class Main
           }
     		}
     	}
-    	
+    	if(c==0)
+        {
+            System.out.println(-1);
+            return;
+        }
     	System.out.println(c+"\n"+ans);
       /*solution end here*/
     }
